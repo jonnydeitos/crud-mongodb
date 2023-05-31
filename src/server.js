7d21a86e78dacc3754b75f50ca72885368318e17
Jonny Deitos <jonny@Jonnys-iMac.local>
2023-05-31 16:39:29 -0300

    add de novas feactures

--- src/server.js
+++ src/server.js
@@ -1,31 +1,32 @@
 const express = require('express')
 const path = require('path')
 
+const db = require('./database')
+const routes = require('./routes')
+
 const app = express()
 
-//definindo o template engine
+// conexão com o banco de dados
+db.connect()
+
+// definindo o template engine
 app.set('view engine', 'ejs')
-//como foi trocado e incluido a pasta src
 app.set('views', path.join(__dirname, 'views'))
 
-//definindo os arquivos públicos
+// definindo os arquivos públicos
 app.use(express.static(path.join(__dirname, 'public')))
 
-//habilita server para receber dados via post (formulários)
-app.use(express.urlencoded({ extended: true}))
+// habilita server para receber dados via post (formulário)
+app.use(express.urlencoded({ extended: true }))
 
-//rotas
-app.get('/', (req, res) =>{
-  res.render('index', {
-    title: 'Título Teste'
-  })
-})
+// definindo as rotas
+app.use('/', routes)
 
-//404 error (not found)
-app.use((req,res) =>{//middleware
+// 404 error (not found)
+app.use((req, res) => { // middleware
   res.send('Página não encontrada!')
 })
 
-//executando o servidor
-const port = process.env.port || 8080
+// executando o servidor
+const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
